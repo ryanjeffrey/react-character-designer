1d612479d71c1901bcfbd13bb41f30a0c21ee903
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-14 19:52:22 -0500

    Import Slogan and Stats into Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Controls from '../Controls/Controls.js';
 import Player from '../Player/Player.js';
+import Slogan from '../Slogan/Slogan.js';
+import Stats from '../Stats/Stats.js';
 
 import './Main.css';
 
@@ -9,6 +11,8 @@ export default function Main() {
     <main>
       <Controls />
       <Player />
+      <Stats />
+      <Slogan />
     </main>
   );
 }
