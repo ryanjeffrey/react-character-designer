5b6f2b3abf157d329828a6033eecb8461b51adad
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-15 11:35:39 -0500

    Create Header component and add it to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Main from './components/Main/Main';
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
     </div>
   );
