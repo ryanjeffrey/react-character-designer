734c98a568ea4072fc4cbd80139e65e51e69fc79
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-14 19:52:03 -0500

    Create rfc for Stats

---
+++ src/components/Stats/Stats.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Stats.css';
+
+export default function Stats() {
+  return (
+    <div className="stats">
+      <p>You have changed the helmet X times.</p>
+      <p>You have changed the jersey X times.</p>
+      <p>You have changed the pants X times.</p>
+      <h3>Slogans</h3>
+      <p>Bear Down!</p>
+    </div>
+  );
+}
