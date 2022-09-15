968fa3e17fb55b0f7f3e56305860483eb945e7bd
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-14 19:09:25 -0500

    Import Player into Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Player from '../Player/Player.js';
+
+import './Main.css';
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <main>
+      <Player />
+    </main>
   );
 }
