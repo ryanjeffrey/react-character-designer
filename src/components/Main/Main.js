c411ad36d63563d5e84c0627e584ce0b300afee2
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-14 19:36:12 -0500

    Import Controls component and style

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Controls from '../Controls/Controls.js';
 import Player from '../Player/Player.js';
 
 import './Main.css';
@@ -6,6 +7,7 @@ import './Main.css';
 export default function Main() {
   return (
     <main>
+      <Controls />
       <Player />
     </main>
   );
